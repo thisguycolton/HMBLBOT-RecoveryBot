7b5624f62688e39d3682fc6f801b2c46f675d1ca
thisguycolton <coltonhagan@gmail.com>
2025-08-26 20:20:07 -0700

    Books on React

--- db/migrate/20250722050246_create_topic_sets.rb
+++ db/migrate/20250722050246_create_topic_sets.rb
@@ -1,21 +1,23 @@
 class CreateTopicSets < ActiveRecord::Migration[8.0]
+  # lightweight model for use in this migration only
+  class TopicSet < ApplicationRecord
+    self.table_name = "topic_sets"
+  end
+
   def change
     create_table :topic_sets do |t|
-      t.string :name
+      t.string :name,        null: false
       t.string :description
-
       t.timestamps
     end
+    add_index :topic_sets, :name, unique: true
 
     reversible do |dir|
       dir.up do
-        # Define a minimal model for use in migration
-        class TopicSet < ActiveRecord::Base; end
-
-        TopicSet.find_or_create_by!(
-          name: "Original Topics",
-          description: "The topics from v1"
-        )
+        TopicSet.reset_column_information
+        TopicSet.find_or_create_by!(name: "Original Topics") do |ts|
+          ts.description = "The topics from v1"
+        end
       end
     end
   end
